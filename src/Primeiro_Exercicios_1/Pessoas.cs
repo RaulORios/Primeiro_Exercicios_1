9c1c58d998b1c10086dda457c07c82ca5c2e4b13
RaulORios <raul.orios89@outlook.com>
2021-09-20 14:48:38 -0300

    Arquivos atualizados

---
+++ src/Primeiro_Exercicios_1/Pessoas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primeiro_Exercicios_1
+{
+    class Pessoas
+    {
+        public string nome;
+        public int idade;
+    }
+}
---
+++ src/Primeiro_Exercicios_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Primeiro_Exercicios_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Pessoas dados1 = new Pessoas();
+            Pessoas dados2 = new Pessoas();
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.Write("Nome : ");
+            dados1.nome = Console.ReadLine();
+            Console.Write("Idade : ");
+            dados1.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Console.Write("Nome : ");
+            dados2.nome = Console.ReadLine();
+            Console.Write("Idade : ");
+            dados2.idade = int.Parse(Console.ReadLine());
+
+            if (dados1.idade > dados2.idade)
+            {
+                Console.WriteLine($"Pessoa mais velha: {dados1.nome}");
+            }
+            else
+            {
+                Console.WriteLine($"Pessoa mais velha: {dados2.nome}");
+            }            
+
+            Console.ReadKey();
+
+        }
+    }
+}
